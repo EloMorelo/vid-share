209d459c72913e838353760dfcb56118253991ab
EloMorelo <jakub0zietek@gmail.com>
2024-09-04 21:52:15 +0200

    first commit

---
+++ src/main/java/com/vd/vid_share/VidShareApplication.java
@@ -0,0 +1,13 @@
+package com.vd.vid_share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vd/vid_share/VidShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vd.vid_share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
